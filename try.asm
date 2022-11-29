f1adbe3d6d5f809c5fc7dc7b35acac031ddc272d
joey9901 <494640@student.saxion.nl>
2022-11-29 15:42:10 +0100

    Update try.asm

--- try.asm
+++ try.asm
@@ -1,9 +1,8 @@
 section .data
-    text1 db "What is your name? "
-    text2 db "Hello, "
- 
+    text1 db "Input operation: "
+
 section .bss
-    name resb 16
+    operation resb 16
  
 section .text
     global _start
@@ -11,8 +10,7 @@ _start:
  
     call _printText1
     call _getName
-    call _printText2
-    call _printName
+    call _printOperation
  
     mov rax, 60
     mov rdi, 0
@@ -21,7 +19,7 @@ _start:
 _getName:
     mov rax, 0
     mov rdi, 0
-    mov rsi, name
+    mov rsi, operation
     mov rdx, 16
     syscall
     ret
@@ -30,22 +28,14 @@ _printText1:
     mov rax, 1
     mov rdi, 1
     mov rsi, text1
-    mov rdx, 19
+    mov rdx, 17
     syscall
     ret
- 
-_printText2:
+
+_printOperation:
     mov rax, 1
     mov rdi, 1
-    mov rsi, text2
-    mov rdx, 7
-    syscall
-    ret
- 
-_printName:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, name
+    mov rsi, operation
     mov rdx, 16
     syscall
     ret
